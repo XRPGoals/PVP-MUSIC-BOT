78ebc8df4c6c26bbe88578993b356ca5fd290edf
XRPGoals <jobbmejl901215@gmail.com>
2026-02-21 21:29:39 +0100

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,1 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+import { skip } from "../music/player.js";
 
+export const data = new SlashCommandBuilder()
+  .setName("skip")
+  .setDescription("Skip the current track");
+
+export async function execute(interaction) {
+  skip(interaction.guildId);
+  await interaction.reply("Skipped.");
+}
