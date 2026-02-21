fcd8ee750f733bb3e074e0d97acfb20c5413e166
XRPGoals <jobbmejl901215@gmail.com>
2026-02-21 21:31:19 +0100

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -1,1 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+import { pause } from "../music/player.js";
 
+export const data = new SlashCommandBuilder()
+  .setName("pause")
+  .setDescription("Pause playback");
+
+export async function execute(interaction) {
+  pause(interaction.guildId);
+  await interaction.reply("Paused.");
+}
