381657225a97cf3b15e438afb616ec76e550d4ba
XRPGoals <jobbmejl901215@gmail.com>
2026-02-21 21:29:56 +0100

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,1 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+import { stop } from "../music/player.js";
 
+export const data = new SlashCommandBuilder()
+  .setName("stop")
+  .setDescription("Stop playback and clear the queue");
+
+export async function execute(interaction) {
+  stop(interaction.guildId);
+  await interaction.reply("Stopped and cleared the queue.");
+}
