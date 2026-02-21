65dac9690d0adfbf0272ffb38681216e3722b23b
XRPGoals <jobbmejl901215@gmail.com>
2026-02-21 21:32:45 +0100

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -1,1 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+import { resume } from "../music/player.js";
 
+export const data = new SlashCommandBuilder()
+  .setName("resume")
+  .setDescription("Resume playback");
+
+export async function execute(interaction) {
+  resume(interaction.guildId);
+  await interaction.reply("Resumed.");
+}
